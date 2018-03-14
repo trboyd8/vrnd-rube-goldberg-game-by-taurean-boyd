3a27b4d339bc0f84871115eb9ce49b048d8388ab
Taurean Boyd <trboyd8@gmail.com>
2018-03-14 00:27:14 -0400

    Adding a script that resets the ball's position when it hits the ground.

---
+++ Assets/Scripts/BallReset.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallReset : MonoBehaviour
+{
+    private Vector3 originalPosition;
+
+	// Use this for initialization
+	void Start ()
+    {
+        originalPosition = this.gameObject.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        
+	}
+
+    // react on collisions
+    void OnCollisionEnter (Collision col)
+    {
+        if (col.gameObject.CompareTag ("Ground"))
+        {
+            Debug.Log("The ball is being reset.");
+            this.gameObject.transform.position = originalPosition;
+        }
+    }
+}
