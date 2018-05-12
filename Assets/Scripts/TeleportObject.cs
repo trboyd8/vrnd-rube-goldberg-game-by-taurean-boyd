106b5eadb573782021d1d3ff1b36c4dc01d91ab8
Taurean Boyd <trboyd8@gmail.com>
2018-05-12 14:41:25 -0400

    Adding teleport system.

---
+++ Assets/Scripts/TeleportObject.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class TeleportObject : MonoBehaviour {
+
+    public GameObject otherTeleporter;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Throwable"))
+        {
+            other.transform.position = otherTeleporter.transform.position;
+        }
+    }
+}
