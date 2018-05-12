f1baa2c9546aab1156642199bc4c0f6e90a00d69
Taurean Boyd <trboyd8@gmail.com>
2018-05-12 13:53:32 -0400

    Adding in physics capabilities of the fan.

--- Assets/Scripts/Fan.cs
+++ Assets/Scripts/Fan.cs
@@ -4,14 +4,12 @@ using UnityEngine;
 
 public class Fan : MonoBehaviour {
 
-    // This class does 2 things. One: It will handle the collision with the ball and provide a force (to simulate a fan).
-    // Two: It will animate the fan blades
     private GameObject fanBlades;
-
+    
 	// Use this for initialization
 	void Start () {
         this.fanBlades = this.gameObject.transform.GetChild(0).gameObject;
-	}
+    }
 	
 	// Update is called once per frame
 	void Update () {
---
+++ Assets/Scripts/Launcher.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Launcher : MonoBehaviour {
+
+    private float thrustAmount = 1000.0f;
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.gameObject.tag);
+        if (other.gameObject.CompareTag("Throwable"))
+        {
+            Rigidbody rigidbody = other.gameObject.GetComponent<Rigidbody>();
+            rigidbody.AddForce(this.transform.forward * this.thrustAmount);
+        }
+    }
+}
