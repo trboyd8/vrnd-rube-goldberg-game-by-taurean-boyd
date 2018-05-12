da543c156c6e9136fc73257ed6f98f54c751add8
Taurean Boyd <trboyd8@gmail.com>
2018-05-12 13:01:07 -0400

    Rotating the platform. Moving the goal to the initial start point. Creating a collectable prefab with a collider and tag.

--- Assets/Scripts/BallReset.cs
+++ Assets/Scripts/BallReset.cs
@@ -21,12 +21,20 @@ public class BallReset : MonoBehaviour
     // react on collisions
     void OnCollisionEnter (Collision col)
     {
+        Debug.Log(col.gameObject.tag);
+
         if (col.gameObject.CompareTag ("Ground"))
         {
             this.gameObject.transform.position = originalPosition;
             Rigidbody ballRigidBody = this.GetComponent<Rigidbody> ();
             ballRigidBody.velocity = Vector3.zero;
             ballRigidBody.angularVelocity = Vector3.zero;
+        }
+        
+        if (col.gameObject.CompareTag("Collectable"))
+        {
+            col.gameObject.SetActive(false);
+            // Need to notify something that the collectable is collected.
         }
     }
 }
