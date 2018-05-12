1d9faed31e7f1d3809ac7092b1ec2685601cac57
Taurean Boyd <trboyd8@gmail.com>
2018-05-12 15:47:28 -0400

    Adding teleport script.

--- Assets/Scripts/TeleportObject.cs
+++ Assets/Scripts/TeleportObject.cs
@@ -3,12 +3,24 @@
 public class TeleportObject : MonoBehaviour {
 
     public GameObject otherTeleporter;
+    public bool canTeleport = true;
 
-    private void OnTriggerEnter(Collider other)
+    void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Throwable"))
+        if (other.gameObject.CompareTag("Throwable")  && this.canTeleport)
         {
+            otherTeleporter.GetComponent<TeleportObject>().canTeleport = false;
             other.transform.position = otherTeleporter.transform.position;
+            Rigidbody rigidbody = other.gameObject.GetComponent<Rigidbody>();
+            rigidbody.velocity = otherTeleporter.transform.forward * rigidbody.velocity.magnitude;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (!this.canTeleport)
+        {
+            this.canTeleport = true;
         }
     }
 }
