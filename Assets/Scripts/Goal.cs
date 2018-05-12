78aa32c0408ec9d53d9d4f5b6f97c30d3e5d0cc0
Taurean Boyd <trboyd8@gmail.com>
2018-05-12 17:25:32 -0400

    Adding a script to the goal for tracking if we are reloading the level. Adding a scene manager script with some default debug logging.

---
+++ Assets/Scripts/Goal.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour {
+
+    public GameObject collectables;
+    public SceneManager sceneManager;
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.CompareTag("Throwable"))
+        {
+            bool foundActiveCollectable = false;
+            foreach (Transform child in collectables.transform)
+            {
+                if (child.gameObject.activeSelf)
+                {
+                    foundActiveCollectable = true;
+                    break;
+                }
+            }
+
+            if (foundActiveCollectable)
+            {
+                sceneManager.ResetLevel();
+            }
+            else
+            {
+                sceneManager.LoadNextLevel();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour {
+
+    public void ResetLevel()
+    {
+        Debug.Log("Resetting the level.");
+    }
+
+    public void LoadNextLevel()
+    {
+        Debug.Log("Loading the next level.");
+    }
+}
